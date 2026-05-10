26b02a0f1a350846e2bd33b92d85b7e803cdf242
sanqsd <wpc18518951654@163.com>
2026-05-10 11:08:00 +0100

    Update properties.js

--- src/properties.js
+++ src/properties.js
@@ -2,7 +2,7 @@
  * Name as used as title for the main page
  * @type {string}
  */
-var title = 'Present a review of the visualization system';
+var title = 'Research on the Visualization Platform for Kidney Transplant Evaluation';
 
 /**
  * Relative path to the data directory
@@ -54,8 +54,8 @@ var editable = true;
  * @type {{html: string, id: string}}
  */
 var paper = {
-    html: 'Sample literature collection for <b><a href="http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/eurovis14-star.pdf" target="_blank">SurVis</a></b>',
-    id: 'Beck2016Visual'
+    html: 'This is a collection of literature <b><a href="https://github.com/sanqsd/survis/tree/master" target="_blank">on visual human-computer interaction</a></b>',
+    id: 'Wingfield2024_Review'
 };
 
 /**
