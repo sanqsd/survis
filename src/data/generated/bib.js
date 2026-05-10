b3ad4a765d23618464ba72a1d589e0a92330daa2
sanqsd <wpc18518951654@163.com>
2026-05-10 02:16:51 +0100

    Update bib.js

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,21 +1,21 @@
 var bibtex = "@article{Wingfield2024_Review,\n" +
-"  author = {Wingfield, Laura R. and Salaun, Achille and Khan, Aparajita and Webb, Helena and Zhu, Tingting and Knight, Simon},\n" +
-"  title = {Clinical Decision Support Systems Used in Transplantation: Are They Tools for Success or an Unnecessary Gadget? A Systematic Review},\n" +
-"  journal = {Transplantation},\n" +
-"  year = {2024},\n" +
-"  keywords = {Category:Clinical_Context, Type:Systematic_Review}\n" +
+"author = {Wingfield, Laura R. and Salaun, Achille and Khan, Aparajita and Webb, Helena and Zhu, Tingting and Knight, Simon},\n" +
+"title = {Clinical Decision Support Systems Used in Transplantation: Are They Tools for Success or an Unnecessary Gadget? A Systematic Review},\n" +
+"journal = {Transplantation},\n" +
+"year = {2024},\n" +
+"keywords = {Category:Clinical_Context, Type:Systematic_Review}\n" +
 "}\n\n" +
 "@inproceedings{Wingfield2024_ICTSS,\n" +
-"  author = {Wingfield, Laura R. and Wainwright, Katie and Knight, Simon and Webb, Helena},\n" +
-"  title = {A trusted friend in the middle of the night: End-user perspectives on Artificial Intelligence informed software systems as a decision-making aid for patients and clinicians navigating uncertainty in kidney transplant},\n" +
-"  booktitle = {ICTSS},\n" +
-"  year = {2024},\n" +
-"  keywords = {Category:User_Perspectives, Type:Qualitative_Study}\n" +
+"author = {Wingfield, Laura R. and Wainwright, Katie and Knight, Simon and Webb, Helena},\n" +
+"title = {A trusted friend in the middle of the night: End-user perspectives on Artificial Intelligence informed software systems as a decision-making aid for patients and clinicians navigating uncertainty in kidney transplant},\n" +
+"booktitle = {ICTSS},\n" +
+"year = {2024},\n" +
+"keywords = {Category:User_Perspectives, Type:Qualitative_Study}\n" +
 "}\n\n" +
 "@article{Kotsifa2024,\n" +
-"  author = {Kotsifa, Evgenia and Mavroeidis, Vasileios K.},\n" +
-"  title = {Present and Future Applications of Artificial Intelligence in Kidney Transplantation},\n" +
-"  journal = {Journal of Clinical Medicine},\n" +
-"  year = {2024},\n" +
-"  keywords = {Category:AI_Tech, Category:Kidney_Transplant}\n" +
+"author = {Kotsifa, Evgenia and Mavroeidis, Vasileios K.},\n" +
+"title = {Present and Future Applications of Artificial Intelligence in Kidney Transplantation},\n" +
+"journal = {Journal of Clinical Medicine},\n" +
+"year = {2024},\n" +
+"keywords = {Category:AI_Tech, Category:Kidney_Transplant}\n" +
 "}";
