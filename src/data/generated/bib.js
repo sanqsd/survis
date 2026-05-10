8a702d0de045fe5a2e492cde2b62609b442bdfa0
woxBongo <wpc18518951654@163.com>
2026-05-10 18:02:41 +0100

    050

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,4 +1,4 @@
-﻿const generatedBibEntries = {
+const generatedBibEntries = {
     "Beck2016Visual": {
         "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
         "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
@@ -13,8 +13,7 @@
         "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
         "volume": "22",
         "year": "2016"
-    }
-
+    },
     "Wingfield2024_Review": {
     "title": "Clinical decision support systems used in transplantation: are they tools for success or an unnecessary gadget? A systematic review",
     "author": "Wingfield, Laura R. and Salaun, Achille and Khan, Aparajita and Webb, Helena and Zhu, Tingting and Knight, Simon",
@@ -25,6 +24,6 @@
     "year": "2024",
     "publisher": "LWW",
     "abstract": "Although clinical decision support systems (CDSSs) have been used since the 1970s for a wide variety of clinical tasks including optimization of medication orders, improved documentation, and improved patient adherence, to date, no systematic reviews have been carried out to assess their utilization and efficacy in transplant medicine. The aim of this study is to systematically review studies that utilized a CDSS and assess impact on patient outcomes. A total of 48 articles were identified as meeting the author-derived inclusion criteria, including tools for posttransplant monitoring, pretransplant risk assessment, waiting list management, immunosuppressant management, and interpretation of histopathology. Studies included 15 984 transplant recipients. Tools aimed at helping with transplant patient immunosuppressant management were the most common (19 studies). Thirty-four studies (85%) found an overall clinical benefit following the implementation of a CDSS in clinical practice. Although there are limitations to the existing literature, current evidence suggests that implementing CDSS in transplant clinical settings may improve outcomes for patients. Limited evidence was found using more advanced technologies such as artificial intelligence in transplantation, and future studies should investigate the role of these emerging technologies.",
-    "doi": "10.1097/TP.0000000000004627",
+    "doi": "10.1097/TP.0000000000004627"
     },
 };
