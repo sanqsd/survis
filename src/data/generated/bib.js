d2da9cdd110ac9c9e343b53b223ba7a1451f3074
woxBongo <wpc18518951654@163.com>
2026-05-10 17:56:43 +0100

    050

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -14,4 +14,17 @@
         "volume": "22",
         "year": "2016"
     }
+
+    "Wingfield2024_Review": {
+    "title": "Clinical decision support systems used in transplantation: are they tools for success or an unnecessary gadget? A systematic review",
+    "author": "Wingfield, Laura R. and Salaun, Achille and Khan, Aparajita and Webb, Helena and Zhu, Tingting and Knight, Simon",
+    "journal": "Transplantation",
+    "volume": "108",
+    "number": "1",
+    "pages": "72-99",
+    "year": "2024",
+    "publisher": "LWW",
+    "abstract": "Although clinical decision support systems (CDSSs) have been used since the 1970s for a wide variety of clinical tasks including optimization of medication orders, improved documentation, and improved patient adherence, to date, no systematic reviews have been carried out to assess their utilization and efficacy in transplant medicine. The aim of this study is to systematically review studies that utilized a CDSS and assess impact on patient outcomes. A total of 48 articles were identified as meeting the author-derived inclusion criteria, including tools for posttransplant monitoring, pretransplant risk assessment, waiting list management, immunosuppressant management, and interpretation of histopathology. Studies included 15 984 transplant recipients. Tools aimed at helping with transplant patient immunosuppressant management were the most common (19 studies). Thirty-four studies (85%) found an overall clinical benefit following the implementation of a CDSS in clinical practice. Although there are limitations to the existing literature, current evidence suggests that implementing CDSS in transplant clinical settings may improve outcomes for patients. Limited evidence was found using more advanced technologies such as artificial intelligence in transplantation, and future studies should investigate the role of these emerging technologies.",
+    "doi": "10.1097/TP.0000000000004627",
+    },
 };
